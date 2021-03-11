858622cfa339d1c43c9bd0b0adcaa26d88b4b6c3
sopkojos <sopkojos@msu.edu>
2021-03-11 11:44:59 -0600

    unit test

---
+++ test_bird_migration_project.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 11 10:19:20 2021
+
+@author: Joe
+"""
+
+import pytest
+import bird_migration_project
+import pandas as pd
+
+def test_file_load_good():
+    data = pd.read_csv('arctic_loon_import.csv')
+    assert type(data) == pandas.core.series.Series
